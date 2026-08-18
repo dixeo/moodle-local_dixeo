b76c142bfb43b2f55929da709d485aac0420d15a
Josemaria Bolanos <josemabol@gmail.com>
2026-08-18 11:12:56 -0400

    Add release 1.9.0

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2026081101;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2026081800;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024100700;        // Requires Moodle 4.5+.
 $plugin->component = 'local_dixeo';     // Full name of the plugin.
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.8.0';
+$plugin->release   = '1.9.0';
